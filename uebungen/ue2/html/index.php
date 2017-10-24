379df811a47d0ee39fbe086115730b63be31be3f
cbeer <cbeer@bitexpert.de>
2017-10-24 12:26:37 +0200

    Added ue2

---
+++ uebungen/ue2/html/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Docker - Iprog UE1 HelloWorld</title>
+  <meta name="description" content="Docker - DB Test">
+  <meta name="author" content="Christoph Beer">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
+</head>
+
+
+<body>
+
+
+<div class="site-wrapper"> 
+  
+  <header>header</header> 
+
+  <div class="content">content
+  <?php
+    #database_test
+    #include
+    #include_once("php/database.php");
+    #include "html/HelloWorld.html";
+    #include"html/login.html";
+    #include"html/Bestellseite.html";
+
+
+  ?> 
+
+  </div> 
+
+  <footer>footer</footer>
+
+
+
+</div><!-- site wrapper -->
+
+</body>
+</html>
+<!doctype html>
