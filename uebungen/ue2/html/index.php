84582702aa4ab9b4947e48a44367da646b52a531
beerfekt <christoph.beer@gmx.de>
2017-10-29 18:41:18 +0100

    Updated last changes

---
+++ uebungen/ue2/html/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Docker - Iprog UE1 HelloWorld</title>
+  <meta name="description" content="Docker - DB Test">
+  <meta name="author" content="Christoph Beer">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
+</head>
+
+
+<body>
+    <div class="site-wrapper">
+
+
+
+
+
+
+
+ <?php
+   #database_test
+   #include
+   #include_once("php/database.php");
+   #include "html/HelloWorld.html";
+   #include"html/login.html";
+   include"html/Bestellseite.html";
+
+
+?> 
+
+
+
+
+</div><!-- site wrapper -->
+
+
+</body>
+</html>
+<!doctype html>
