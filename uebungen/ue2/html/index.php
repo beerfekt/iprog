1586cd2630a0c19e8a32087cd2a8db933b9688ff
beerfekt <christoph.beer@gmx.de>
2017-10-29 20:06:14 +0100

    Added Responsivenes

--- uebungen/ue2/html/index.php
+++ uebungen/ue2/html/index.php
@@ -1,43 +1,26 @@
-<!doctype html>
+<!DOCTYPE html>
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
+<html lang="de">
 
-  <title>Docker - Iprog UE1 HelloWorld</title>
-  <meta name="description" content="Docker - DB Test">
-  <meta name="author" content="Christoph Beer">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
-</head>
-
-
-<body>
+  <head>
+    <meta charset="utf-8">
+    <title>Docker - Iprog UE1 HelloWorld</title>
+    <meta name="description" content="Docker - DB Test">
+    <meta name="author" content="Christoph Beer">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
+  </head>
+  <body>
     <div class="site-wrapper">
 
-
-
-
-
-
-
- <?php
-   #database_test
-   #include
-   #include_once("php/database.php");
-   #include "html/HelloWorld.html";
-   #include"html/login.html";
-   include"html/Bestellseite.html";
-
-
-?> 
-
-
-
-
-</div><!-- site wrapper -->
-
-
-</body>
+       <?php
+         #database_test
+         #include
+         #include_once("php/database.php");
+         #include "html/HelloWorld.html";
+         #include"html/login.html";
+         include"html/Bestellseite.html";
+       ?> 
+    </div><!-- site wrapper -->
+ </body>
 </html>
-<!doctype html>
