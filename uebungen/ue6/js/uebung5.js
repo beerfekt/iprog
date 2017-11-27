6dac8bd43f41c9ced0aeb20c0aa63ae30df62083
beerfekt <christoph.beer@gmx.de>
2017-11-27 13:12:17 +0100

    uebung 6 - a1

--- uebungen/ue6/js/uebung5.js
+++ uebungen/ue6/js/uebung5.js
@@ -21,6 +21,7 @@ PROTOTYP = Objekt in javascript (nur bedingt)
 
 //AUFGABE 1)
 
+let telefonBoolean = false;
 
 window.onload = function(){
 	console.log('onload funzt');
@@ -29,21 +30,37 @@ window.onload = function(){
 	let name = document.getElementById('name');
 	let telefon = document.getElementById('telefon');
 	let eMail = document.getElementById('email');
+	let feedback = document.getElementById('feedback');
+
 	let buttSpeichern = document.getElementById('speichern');
 	console.log('variablen eingelesen');
 
 	//eventlistener
-	name.addEventListener('blur', checkName, false);
-/*
-	telefon.addEventListener('blur',checkTelefon ,false);
-	eMail.addEventListener('blur',checkEMail,false);
-*/
-	buttSpeichern.addEventListener('click',doSomething ,false);
+	buttSpeichern.addEventListener('click', speichern ,false);
+	//name.addEventListener('keyup', checkName, false);
+	telefon.addEventListener('change',function (){
+		var regexTelefon =  /^0\d{3,}-[1-9]{1}\d{3,}$/g;
+// /^(0[0,9]{3,})-([1-9]{1}[0-9]{3,})$/g  -> falsch? warum
+		if (this.value.match(regexTelefon)){
+			console.log("telefon richtig");
+			telefonBoolean = true;
+		} else {
+			console.log("telefon falsch");
+			telefonBoolean = false;
+		}
+
+			checkAll(buttSpeichern);
+
+	} ,false);
+	//eMail.addEventListener('keyup',checkEMail,false);
+
+
+
 }
 
 
-function doSomething(){
-	console.log('submit: do something');
+function speichern(){
+	console.log('speichern');
 }
 
 //element.removeAttribute("disabled");
@@ -51,15 +68,34 @@ function doSomething(){
 //element.disabled = false;
 
 
-let nameKorrekt, telefonKorrekt, eMailKorrekt;
 
 // Ein Name beginnt mit einem Großbuchstaben und 
 // endet immer mit einem Kleinbuchstaben.
 
 
 
-function checkName ( name ) {
+/*
+REGEX:
+testen mit: https://regexr.com/
 
+/inhalt/
+/^ anfang bis ende $/
+^ anfang bis open end
+irgendein anfang bis dieses ende $/
+[von-bis]{anzahl der zeichen von-bis?}
+(das|oderdas)
+
+
+
+/^[a-z]{4}[0-9]{4}@stud.(hs|fh)-kl.de$/g
+
+
+
+
+*/
+
+function checkName ( ) {
+/*
 	var regexName = /([\u00c0-\u01ffa-zA-Z\.'\-]{3,})/;
 	var regexTelefon = /01234567890/;
 	var regexeMail = //;
@@ -68,9 +104,9 @@ function checkName ( name ) {
 		console.log('whitelist erfüllt');
 	} else {
 		console.log('whitelist nicht erfüllt');
-	}
+	}//if else
 
-/*
+
 	const letters = {"abcdefghijklmnopqrstuvwxyzäöü-"}.split("");
 
 	for (let i = 0; i < letters.length; i++) {
@@ -79,15 +115,45 @@ function checkName ( name ) {
 		}
 	}
 */
+}//checkName
 
 
+function checkTelefon (){
+   var regexTelefon =  /^0\d{3,}-[1-9]{1}\d{3,}$/g;
+// /^(0[0,9]{3,})-([1-9]{1}[0-9]{3,})$/g  -> falsch? warum
+/*
+	if (formfield.value.match(regexTelefon)){
+		console.log("telefon richtig");
+		return true;
+	} else {
+		console.log("telefon falsch");
+		return false;	
+	}
+*/
+}
 
+//function checkEmail(){}
+
+function checkAll(buttSpeichern){
+	console.log("checkall wird aufgerufen");
+	//wenn jede funktion true liefert:
+	if (telefonBoolean ){
+		console.log('checktelefon funktioniert');
+		buttSpeichern.disabled = false;	
+	} else {
+		buttSpeichern.disabled = true;	
+		console.log('checktelefon funktioniert nicht');	
+	}
 
 }
 
 
-function checkTelefon (){}
 
-function checkEmail(){}
 
+//A2
+
+
+
+
+//A3
 
