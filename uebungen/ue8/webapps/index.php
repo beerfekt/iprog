b7b480c15905b80ac37dc57eb3238ef207d13ba8
beerfekt <christoph.beer@gmx.de>
2018-01-21 11:11:15 +0100

    ue8 hinzugefügt

---
+++ uebungen/ue8/webapps/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Docker - Iprog UE9 - Servlets</title>
+  <meta name="description" content="Docker - Apache-Php-Tomcat">
+  <meta name="author" content="Christoph Beer">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
+</head>
+
+
+<body>
+
+
+<div class="site-wrapper"> 
+  
+  <header>header</header> 
+
+  <div class="content">content
+  <?php
+    #database_test
+    #include
+    include_once("php/database.php");
+    #include "html/HelloWorld.html";
+    #include"html/login.html";
+    #include"html/Bestellseite.html";
+
+
+  ?> 
+
+  </div> 
+
+  <footer>footer</footer>
+
+
+
+</div><!-- site wrapper -->
+
+</body>
+</html>
